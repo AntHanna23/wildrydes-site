3159689b156621719864d0839272074066a3ab7f
websec-anthony <ahanna@anthonyhanna.com>
2025-06-16 22:16:30 -0700

    Update js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_vQzDFQuZk', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6dneitkatd8m5ph7pt0dlqup7p', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
